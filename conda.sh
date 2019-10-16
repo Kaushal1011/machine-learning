d0043ae64159b2d174a35520ffecd24ae0f7b6d7
Dhruvil Dave <dhruvil.dave@outlook.com>
2019-10-16 16:53:51 +0530

    Wed 16 Oct 2019 04:53:51 PM IST -> Added conda.sh

---
+++ conda.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Install the Data Science stack in conda environment
+if [ -x "$(command -v conda)" ]; then
+    conda install -y flask \
+                     ipython \
+                     jupyterlab \
+                     matplotlib \
+                     numpy \
+                     pandas \
+                     pylint \
+                     R \
+                     requests \
+                     scikit-learn \
+                     scipy \
+                     sympy \
+                     tensorflow-gpu \
+                     yapf
+    conda update --all -y
+else
+    echo 'conda not installed'
+    exit 1
+fi
