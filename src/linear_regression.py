32e3c2061c71327aa694aa3ea3132606850c4d40
Dhruvil Dave <dhruvil.dave@outlook.com>
2019-10-18 11:21:11 +0530

    Fri 18 Oct 2019 11:21:11 AM IST -> Updated Linear Regression in Py3

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-Performing Simple Linear Regression
+Performing Simple and Multiple Linear Regression
 https://github.com/dhruvildave
 Thu Oct 17 14:00:50 IST 2019
 '''
@@ -24,15 +24,28 @@ def main() -> None:
     Y = adv['sales']
 
     # Converting pandas Series to numpy ndarray
-    X = np.array(X).reshape((-1, 1))
-    Y = np.array(Y).reshape((-1, 1))
+    X = np.array(X).reshape(-1, 1)
+    Y = np.array(Y).reshape(-1, 1)
 
     # Fitting the linear model
     fit = lm.LinearRegression().fit(X, Y)
 
+    print('Simple Linear Regression')
     print('Intercept == {}'.format(fit.intercept_))
     print('Slope == {}'.format(fit.coef_))
     print('Multiple R^2 == {}'.format(fit.score(X, Y)))
+
+    print('-' * 40)
+
+    # Performing Multiple Linear Regression
+    X = adv.iloc[:, 0:3]
+    Y = adv['sales']
+    mul_fit = lm.LinearRegression().fit(X, Y)
+
+    print('Multiple Linear Regression')
+    print('Intercept == {}'.format(mul_fit.intercept_))
+    print('Slope == {}'.format(mul_fit.coef_))
+    print('Multiple R^2 == {}'.format(mul_fit.score(X, Y)))
 
 
 if __name__ == '__main__':
